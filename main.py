2260aca11e77c423e30ac170fe760b1e74252ebf
valdeortiz <valdeortiz15@gmail.com>
2021-10-17 20:01:10 -0300

    update to click

--- main.py
+++ main.py
@@ -1,49 +1,53 @@
 import logging
-from cmd import cmd
+import click
+import getpass
+
 #archivo = "/var/log" path para lfs
 archivo_usuario = "usuarios_log" # /var/log/usuarios_log
-archivo_personal_horarios = "usuario_horarios_log" # /var/log/(usuario_horarios_log)
-archivo_personal_horarios = "Shell_transferencias" # /var/log/(usuario_horarios_log)
+archivo_personal_horarios = "usuario_horarios.log" # /var/log/(usuario_horarios_log)
+archivo_personal_horarios = "Shell_transferencias.log" # /var/log/(usuario_horarios_log)
 
-class Comandos(cmd):
-    """
-        Clase principal que hereda de la clase Cmd
-        que contiene todo lo necesario para la construccion
-        de la shell.
-    """
-    # Atributos de la clase Cmd
-    intro = "***  Shell  *** \n=> introduzca <help> para visualizar los comandos." #Mensaje de bienvenida al ejecutarse la shell
-    prompt = "Introduza un comando: " #mensaje a la izquierda del comando
-    misc_header="Documentacion de los metodos" 
-    doc_header = "Ayuda de comandos documentados. Presione help <comando>"
-    undoc_header="Los siguientes comandos no estan documentados:"
-    ruler = "*" # caracter separador al ejecutar help=menu de ayuda
+#Creamos nuestro logger principal y usamos el metodo basicConfig para configurar 
+logging.basicConfig( level=logging.INFO,
+                    # formato del horario (YYYY-MM-DD hh:min:sec), 
+                    format='%(asctime)s %(name)s %(levelname)s %(message)s', # name es el user, asctime es hora y fecha, levelname: severidad, message: mensaje del error.
+                    filename="shell.log")
+
+#creamos otro logger para guardar los errores del sistema
+# cambiar el nombre del archivo a el path del log donde se desea guardar los errores
+# los errores de comandos van separados de los errores de inicio de sesion
+log_error = logging.getLogger("")
+fhp = logging.FileHandler("errores.log")
+fhp.setLevel(logging.ERROR)
+log_error.addHandler(fhp)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fhp.setFormatter(formatter)
+log_error.addHandler(fhp)
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+def bienvenida(args):
+    """emite un mensaje de bienvenida """
+    print("Bienvenido!")
+
+@cli.command()
+def salida( args):
+    """Termina el loop y emite un mensaje de despedida """
+    print("Hasta pronto!")
+    return True 
     
-    #Creamos nuestro logger principal y usamos el metodo basicConfig para configurar 
-    logging.basicConfig( level=logging.INFO,
-                        # formato del horario (YYYY-MM-DD hh:min:sec), 
-                        format='%(asctime)s %(name)s %(levelname)s %(message)s', # name es el user, asctime es hora y fecha, levelname: severidad, message: mensaje del error.
-                        filename="shell.log")
+def log( args):
+        logging.info(f"Se ejecuto el comando -- {args}")
 
-    #creamos otro logger para guardar los errores del sistema
-    # cambiar el nombre del archivo a el path del log donde se desea guardar los errores
-    # los errores de comandos van separados de los errores de inicio de sesion
-    log_error = logging.getLogger("")
-    fhp = logging.FileHandler("errores.log")
-    fhp.setLevel(logging.ERROR)
-    log_error.addHandler(fhp)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fhp.setFormatter(formatter)
-    log_error.addHandler(fhp)
-
-    def do_salir(self, args):
-        """Termina el loop y emite un mensaje de despedida """
-        print("Hasta pronto!")
-        return True 
 
 
 if __name__ == '__main__':
-    user = 'Admin'
+    user = getpass.getuser() # capturamos el nombre de usuario de la pc.
+    
+    # creamos un nuevo log con nombre sesion_log
     logger = logging.getLogger(archivo_personal_horarios)
     logger.setLevel(logging.DEBUG)
     fh = logging.FileHandler(archivo_personal_horarios) #/var/log/archivo_personal
@@ -54,7 +58,7 @@ if __name__ == '__main__':
     logger.addHandler(fh)
     logger.info(f'Inicio de sesion de {user}')
     try:
-        Comandos().cmdloop()  # iniciamos el loop para que capture los comandos ingresados 
+        cli()  # iniciamos el loop para que capture los comandos ingresados 
     except KeyboardInterrupt: # en caso de ctrl + c se ejecuta una interrupcion del teclado y se termina la sesion
         print(f"\nCierre de sesion : {user} - Interrupcion de teclado")
         logger.info(f" {user} Cerro sesion por Interrupcion de teclado")
