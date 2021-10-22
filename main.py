b1bf46a8b4d12a79e6368beeaef3655e51601ce5
Alcides Aveiro <70342041+AlcidesAveiro@users.noreply.github.com>
2021-10-22 11:06:11 -0300

    Función ir y copiar agregadas
    Intente modificar la linea 36 del código para que se pueda visualizar en todo momento el directorio actual, según la documentación de click-shell lo que hice esta bien pero no funciona, por el momento salta un mensaje que indica que el cambio de directorio fue exitoso.

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 #! /usr/bin/env python
+import shutil
+import click
 import logging
 from click_shell import shell
 import getpass
+import os
+# archivo = "/var/log" path para lfs
+archivo_usuario = "usuarios_log"  # /var/log/usuarios_log
+archivo_personal_horarios = "usuario_horarios.log"  # /var/log/(usuario_horarios_log)
+archivo_personal_horarios = "Shell_transferencias.log"  # /var/log/(usuario_horarios_log)
 
-#archivo = "/var/log" path para lfs
-archivo_usuario = "usuarios_log" # /var/log/usuarios_log
-archivo_personal_horarios = "usuario_horarios.log" # /var/log/(usuario_horarios_log)
-archivo_personal_horarios = "Shell_transferencias.log" # /var/log/(usuario_horarios_log)
-
-#Creamos nuestro logger principal y usamos el metodo basicConfig para configurar 
-logging.basicConfig( level=logging.INFO,
-                    # formato del horario (YYYY-MM-DD hh:min:sec), 
-                    format='%(asctime)s %(name)s %(levelname)s %(message)s', # name es el user, asctime es hora y fecha, levelname: severidad, message: mensaje del error.
+# Creamos nuestro logger principal y usamos el metodo basicConfig para configurar
+logging.basicConfig(level=logging.INFO,
+                    # formato del horario (YYYY-MM-DD hh:min:sec),
+                    format='%(asctime)s %(name)s %(levelname)s %(message)s',
+                    # name es el user, asctime es hora y fecha, levelname: severidad, message: mensaje del error.
                     filename="shell.log")
 
-#creamos otro logger para guardar los errores del sistema
+# creamos otro logger para guardar los errores del sistema
 # cambiar el nombre del archivo a el path del log donde se desea guardar los errores
 # los errores de comandos van separados de los errores de inicio de sesion
 log_error = logging.getLogger("")
@@ -25,9 +28,15 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 fhp.setFormatter(formatter)
 log_error.addHandler(fhp)
 
-@shell(prompt='shell> ', intro='**** Bienvenido ***')
+
+#UpdatePrompt=os.getcwd()
+def UpdatePrompt():#Esto no funciona por alguna razon
+    return f"shell>{os.getcwd()}>"
+
+@shell(prompt=f'[{UpdatePrompt()}]>', intro='**** Bienvenido ***')
 def cli():
     pass
+
 
 @cli.command()
 def bienvenida():
@@ -35,23 +44,48 @@ def bienvenida():
     print("Bienvenido!")
     pass
 
+
 @cli.command()
 def salida():
     """Termina el loop y emite un mensaje de despedida """
     print("Hasta pronto!")
-    return True 
-    
+    return True
+
+@cli.command()
+@click.argument('path')
+def ir(path):
+    """Cambio de Directorio."""
+    try:
+        os.chdir(path)
+        print("Directorio Actual: ", os.getcwd())
+    except:
+        print("No es posible acceder al directorio o no existe.")
+
+@cli.command()
+@click.argument('SOURCE', type=click.Path(exists=True))
+@click.argument('DESTINATION', type=click.Path(exists=True))
+def copiar(SOURCE,DESTINATION):
+    """Copia un archivo a un directorio."""
+    try:
+        shutil.copy(SOURCE, DESTINATION)
+        print(f"Se copio {click.format_filename(SOURCE)} a {click.format_filename(DESTINATION)} con exito.")
+    except:
+        print("Error al copiar documentos, no tiene los permisos necesarios o error al especificar la ubicación"
+              "del archivo o directorio")
+
+
+
 def log(args):
-        logging.info(f"Se ejecuto el comando -- {args}")
+    logging.info(f"Se ejecuto el comando -- {args}")
 
 
 if __name__ == '__main__':
-    user = getpass.getuser() # capturamos el nombre de usuario de la pc.
-    
+    user = getpass.getuser()  # capturamos el nombre de usuario de la pc.
+
     # creamos un nuevo log con nombre sesion_log
     logger = logging.getLogger(archivo_personal_horarios)
     logger.setLevel(logging.DEBUG)
-    fh = logging.FileHandler(archivo_personal_horarios) #/var/log/archivo_personal
+    fh = logging.FileHandler(archivo_personal_horarios)  # /var/log/archivo_personal
     fh.setLevel(logging.DEBUG)
     logger.addHandler(fh)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -59,12 +93,12 @@ if __name__ == '__main__':
     logger.addHandler(fh)
     logger.info(f'Inicio de sesion de {user}')
     try:
-        cli()  # iniciamos el loop para que capture los comandos ingresados 
-    except KeyboardInterrupt: # en caso de ctrl + c se ejecuta una interrupcion del teclado y se termina la sesion
+        cli()  # iniciamos el loop para que capture los comandos ingresados
+    except KeyboardInterrupt:  # en caso de ctrl + c se ejecuta una interrupcion del teclado y se termina la sesion
         print(f"\nCierre de sesion : {user} - Interrupcion de teclado")
         logger.info(f" {user} Cerro sesion por Interrupcion de teclado")
         exit()
-    else: # en cualquier caso. a la hora de salida se informa del cierre de sesion
+    else:  # en cualquier caso. a la hora de salida se informa del cierre de sesion
         logger.info(f"Cierre de sesion : {user}")
 
 
